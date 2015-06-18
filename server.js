38d22cd2525daf0ec3edd7158b4774f5f273b19b
Dag Nygaard <dn@sekila.com>
2015-06-18 12:33:24 +0200

    code cleanup

--- server.js
+++ server.js
@@ -10,38 +10,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());	
 
-/*
-var connection_string = '127.0.0.1:27017/ndc';
-if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
- 	console.log("Using remote DB");
-	connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" 
-	                  + process.env.OPENSHIFT_MONGODB_PASSWORD    + "@" 
-					  + process.env.OPENSHIFT_MONGODB_HOST        + ':' 
-					  + process.env.OPENSHIFT_MONGODB_PORT        + '/' 
-					  + process.env.OPENSHIFT_APP_NAME
-};
-*/
-
-//var db = mongojs('username:password@example.com/mydb', ['mycollection']);
-
 var connection_string = process.env.OPENSHIFT_MONGODB_DB_URL || '127.0.0.1:27017/ndc';
-
-
-
 var mongojs = require('mongojs');
-//var db = mongojs('ndc', ['utfordrer']);
 var db = mongojs(connection_string, ['utfordrer']);
 
-
-app.get('/foo', function (req, res) {
-	res.json(process.env);
-});
 	
 app.get('/', function (req, res) {
-	res.send('Sanity check version 7' + process.env.OPENSHIFT_MONGODB_HOST        + ':' 
-					  + process.env.OPENSHIFT_MONGODB_PORT        + '/' 
-					  + process.env.OPENSHIFT_APP_NAME);
+	res.send('Sanity check version 8');
 });
+
 app.get('/api/challenge', function(req, res) {
 	db.utfordrer.find().toArray(function(err, items) {
 		if (err) {
