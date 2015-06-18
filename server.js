06e15d9dd995518ccf545a1a37a0159e5d9b4597
Dag Nygaard <dn@sekila.com>
2015-06-18 08:02:57 +0200

    add mongodb setup

--- server.js
+++ server.js
@@ -4,28 +4,43 @@ var cc          = require('config-multipaas'),
     express     = require('express');
 
 var config      = cc(),
-    app = express();
+    app         = express();
 	
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());	
 
+var connection_string = '127.0.0.1:27017/ndc';
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+ 	console.log("Using remote DB");
+	connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" 
+	                  + process.env.OPENSHIFT_MONGODB_PASSWORD    + "@" 
+					  + process.env.OPENSHIFT_MONGODB_HOST        + ':' 
+					  + process.env.OPENSHIFT_MONGODB_PORT        + '/' 
+					  + process.env.OPENSHIFT_APP_NAME
+};
+
+var mongojs = require('mongojs');
+//var db = mongojs('ndc', ['utfordrer']);
+var db = mongojs(connection_string, ['utfordrer']);
+
 
 app.get('/', function (req, res) {
-	res.send('hallo verden 2');
+	res.send('Sanity check version 4');
 });
-
-//app.use(express.static(__dirname + '/public')); // supply access ot ./public/index.html ++
 
 app.get('/api/challenge', function(req, res) {
-	res.send('list all ');
-});
+	db.utfordrer.find().toArray(function(err, items) {
+		res.send(items);
+	});
+})
 
 app.post('/api/challenge', function(req, res) {
-	res.send('add new  challenger');
+	res.send('TODO add new  challenger');
 });
 
+
 app.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+  console.log("Listening on " + config.get('IP') + ", port " + config.get('PORT') )
 });
 
